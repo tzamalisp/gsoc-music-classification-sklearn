f3e57f079582d2417c0861048428f9ba27eb07ac
Pantelis Tzamalis <tzamalispantelis@gmail.com>
2020-08-01 16:02:57 +0300

    predictions logging

--- predict.py
+++ predict.py
@@ -12,9 +12,10 @@ from logging_tool import LoggerSetup
 
 
 class Predict:
-    def __init__(self, config, track_low_level):
+    def __init__(self, config, track_low_level, log_level):
         self.config = config
         self.track_low_level = track_low_level
+        self.log_level = log_level
 
         self.class_name = ""
         self.exports_path = ""
@@ -23,18 +24,11 @@ class Predict:
         self.track_feats = dict()
 
         self.load_best_model()
+        # self.setting_logger()
+        self.logger = ""
         # self.flat_dict()
         self.df_track = pd.DataFrame()
         self.list_track = []
-
-    # def setting_logger(self):
-    #     # set up logger
-    #     self.logger = LoggerSetup(config=self.config,
-    #                               exports_path=self.exports_path,
-    #                               name="dataset_exports_transformations_{}".format(self.train_class),
-    #                               train_class=self.train_class,
-    #                               mode="a",
-    #                               level=self.log_level).setup_logger()
 
     def load_best_model(self):
         self.class_name = self.config["class_name"]
@@ -48,44 +42,54 @@ class Predict:
         # best_model_path = os.path.join(self.exports_dir, "models", "model_grid_{}.pkl".format[""])
         with open(best_model_path) as json_file:
             self.best_model = json.load(json_file)
-        print("Best model:")
-        pprint(self.best_model)
 
     def preprocessing(self):
-        print("FLATTENING:")
+        # set up logger
+        self.logger = LoggerSetup(config=self.config,
+                                  exports_path=self.exports_path,
+                                  name="predict_{}".format(self.class_name),
+                                  train_class=self.class_name,
+                                  mode="w",
+                                  level=self.log_level).setup_logger()
+
+        self.logger.info("Best model:")
+        self.logger.info(self.best_model)
+
+        self.logger.info("FLATTENING:")
         try:
             if 'beats_position' in self.track_low_level['rhythm']:
                 del self.track_low_level['rhythm']['beats_position']
         except Exception as e:
-            print("There is no 'rhythm' key in the low level data. Exception:", e)
+            self.logger.warning("There is no 'rhythm' key in the low level data. Exception:", e)
 
         # data dictionary transformed to a fully flattened dictionary
         self.track_feats = dict(flatten_dict_full(self.track_low_level))
         list_track = []
         list_track.append(self.track_feats)
-        print("DICT TO DATAFRAME:")
+        self.logger.debug("DICT TO DATAFRAME:")
         self.df_track = pd.DataFrame(data=list_track, columns=list_track[0].keys())
-        print("TYPE:", type(self.df_track))
+        self.logger.debug("TYPE of track structure: {}".format(type(self.df_track)))
         # print(self.df_track)
         # print("Shape of DF", self.df_track.shape)
 
-        print("PROCESSING:")
+        self.logger.info("PROCESSING:")
         features_prepared = TransformPredictions(config=self.config,
                                                  df_feats=self.df_track,
                                                  process=self.best_model["preprocessing"],
                                                  train_class=self.class_name,
-                                                 exports_path=self.exports_path
+                                                 exports_path=self.exports_path,
+                                                 log_level=self.log_level
                                                  ).post_processing()
-        print(features_prepared.shape)
+        self.logger.debug("Features shape after preparation: {}".format(features_prepared.shape))
         models_path = FindCreateDirectory(self.exports_path,
                                           os.path.join(self.exports_dir, "models")).inspect_directory()
         best_model_path = os.path.join(models_path, "model_grid_{}.pkl".format(self.best_model["preprocessing"]))
         clf_loaded = joblib.load(best_model_path)
         predicted = clf_loaded.predict(features_prepared)
         predicted_prob = clf_loaded.predict_proba(features_prepared)
-        print("Prediction:", predicted)
-        print("Classes: ", clf_loaded.classes_)
-        print("Prediction probabilities", predicted_prob)
+        self.logger.info("Prediction: {}".format(predicted))
+        self.logger.info("Classes: {}".format(clf_loaded.classes_))
+        self.logger.info("Prediction probabilities: {}".format(predicted_prob))
         predict_list = []
         for pred, pred_probability in zip(predicted, predicted_prob):
             predict_dict = dict()
@@ -95,12 +99,14 @@ class Predict:
 
             predict_list.append(predict_dict)
 
-        pprint(predict_list)
+        self.logger.info("")
+        self.logger.info("{}".format(predict_list))
+        self.logger.info("predict_list")
 
         return predict_list
 
 
-def prediction(exports_path, project_file, track_api):
+def prediction(exports_path, project_file, track_api, log_level):
     # if empty, path is declared as the app's main directory
 
     if exports_path is None:
@@ -121,7 +127,8 @@ def prediction(exports_path, project_file, track_api):
         print("Track:", track["metadata"]["tags"]["album"][0])
 
     prediction_track = Predict(config=project_data,
-                               track_low_level=track
+                               track_low_level=track,
+                               log_level=log_level
                                )
     prediction_track.preprocessing()
 
@@ -145,8 +152,15 @@ if __name__ == '__main__':
                         help='Low-level data link from the AcousticBrainz API.',
                         required=True)
 
+    parser.add_argument('-l', '--logging',
+                        dest='log_level',
+                        default=1,
+                        help='Path where the result files will be stored.',
+                        type=int)
+
     args = parser.parse_args()
 
     prediction(exports_path=args.exports_path,
                project_file=args.project_file,
-               track_api=args.track_api)
+               track_api=args.track_api,
+               log_level=args.log_level)
--- transformation/transform_predictions.py
+++ transformation/transform_predictions.py
@@ -8,6 +8,7 @@ from utils import FindCreateDirectory
 from transformation.utils_preprocessing import list_descr_handler
 from transformation.utils_preprocessing import feats_selector_list
 from sklearn.base import BaseEstimator, TransformerMixin
+from logging_tool import LoggerSetup
 
 # avoid the module's method call deprecation
 try:
@@ -17,25 +18,38 @@ except AttributeError:
 
 
 class TransformPredictions:
-    def __init__(self, config, df_feats, process, train_class, exports_path):
+    def __init__(self, config, df_feats, process, train_class, exports_path, log_level):
         self.config = config
         self.df_feats = df_feats
         self.process = process
         self.train_class = train_class
         self.exports_path = exports_path
+        self.log_level = log_level
 
+        self.logger = ""
         self.list_features = []
         self.feats_cat_list = []
         self.feats_num_list = []
 
         self.feats_prepared = []
 
+        self.setting_logger()
+
+    def setting_logger(self):
+        # set up logger
+        self.logger = LoggerSetup(config=self.config,
+                                  exports_path=self.exports_path,
+                                  name="predict_{}".format(self.train_class),
+                                  train_class=self.train_class,
+                                  mode="a",
+                                  level=self.log_level).setup_logger()
+
     def post_processing(self):
         print(colored("PROCESS: {}".format(self.process), "cyan"))
         # list_preprocesses = []
 
-        print(self.df_feats)
-        print("Shape of DF:", self.df_feats.shape)
+        self.logger.debug("Track Features - Low Level: {}".format(self.df_feats))
+        self.logger.debug("Shape of DF: {}".format(self.df_feats.shape))
 
         self.list_features = list(self.df_feats.columns)
 
@@ -46,10 +60,10 @@ class TransformPredictions:
         # clean list
         print(colored("Cleaning..", "yellow"))
         cleaning_conf_list = list_descr_handler(self.config["excludedDescriptors"])
-        print("cleaning list:", cleaning_conf_list)
+        self.logger.debug("cleaning list: {}".format(cleaning_conf_list))
         feats_clean_list = feats_selector_list(self.df_feats.columns, cleaning_conf_list)
         self.list_features = [x for x in self.df_feats.columns if x not in feats_clean_list]
-        print("List after cleaning some feats: {}".format(len(self.list_features), "blue"))
+        self.logger.debug("List after cleaning some feats: {}".format(len(self.list_features), "blue"))
 
         # remove list
         print(colored("Removing unnecessary features..", "yellow"))
@@ -57,22 +71,22 @@ class TransformPredictions:
             remove_list = list_descr_handler(self.config["processing"][self.process][0]["params"]["descriptorNames"])
             feats_remove_list = feats_selector_list(self.df_feats.columns, remove_list)
             self.list_features = [x for x in self.list_features if x not in feats_remove_list]
-            print("List after removing unnecessary feats: {}".format(len(self.list_features), "blue"))
+            self.logger.debug("List after removing unnecessary feats: {}".format(len(self.list_features), "blue"))
 
         # enumerate list
         print(colored("Removing unnecessary features..", "yellow"))
         if self.config["processing"][self.process][1]["transfo"] == "enumerate":
             enumerate_list = list_descr_handler(self.config["processing"][self.process][1]["params"]["descriptorNames"])
             self.feats_cat_list = feats_selector_list(self.list_features, enumerate_list)
-            print("Enumerating feats: {}".format(self.feats_cat_list))
+            self.logger.debug("Enumerating feats: {}".format(self.feats_cat_list))
             self.feats_num_list = [x for x in self.list_features if x not in self.feats_cat_list]
-            print("List Num feats: {}".format(len(self.feats_num_list)))
-            print("List Cat feats: {}".format(len(self.feats_cat_list), "blue"))
+            self.logger.debug("List Num feats: {}".format(len(self.feats_num_list)))
+            self.logger.debug("List Cat feats: {}".format(len(self.feats_cat_list), "blue"))
 
         # BASIC
         if self.process == "basic":
             print(colored("Process doing: {}".format(self.process), "green"))
-            print("List post-Num feats: {}".format(len(self.feats_num_list)))
+            self.logger.debug("List post-Num feats: {}".format(len(self.feats_num_list)))
 
             # load pipeline
             full_pipeline = joblib.load(os.path.join(models_path, "full_pipeline_{}.pkl".format(self.process)))
@@ -84,7 +98,7 @@ class TransformPredictions:
             print(colored("Process doing: {}".format(self.process), "green"))
             sel_list = list_descr_handler(self.config["processing"][self.process][2]["params"]["descriptorNames"])
             self.feats_num_list = feats_selector_list(self.feats_num_list, sel_list)
-            print("List post-Num feats: {}".format(len(self.feats_num_list)))
+            self.logger.debug("List post-Num feats: {}".format(len(self.feats_num_list)))
 
             # load pipeline
             full_pipeline = joblib.load(os.path.join(models_path, "full_pipeline_{}.pkl".format(self.process)))
@@ -97,7 +111,7 @@ class TransformPredictions:
             sel_list = list_descr_handler(self.config["processing"][self.process][2]["params"]["descriptorNames"])
             feats_rem_list = feats_selector_list(self.df_feats, sel_list)
             self.feats_num_list = [x for x in self.feats_num_list if x not in feats_rem_list]
-            print("List post-Num feats: {}".format(len(self.feats_num_list)))
+            self.logger.debug("List post-Num feats: {}".format(len(self.feats_num_list)))
 
             # load pipeline
             full_pipeline = joblib.load(os.path.join(models_path, "full_pipeline_{}.pkl".format(self.process)))
@@ -107,7 +121,7 @@ class TransformPredictions:
         # NORMALIZED
         if self.process == "normalized":
             print(colored("Process doing: {}".format(self.process), "green"))
-            print("List post-Num feats: {}".format(len(self.feats_num_list)))
+            self.logger.debug("List post-Num feats: {}".format(len(self.feats_num_list)))
 
             # load pipeline
             full_pipeline = joblib.load(os.path.join(models_path, "full_pipeline_{}.pkl".format(self.process)))
@@ -121,8 +135,8 @@ class TransformPredictions:
             feats_num_gauss_list = feats_selector_list(self.feats_num_list, gauss_list)
             feats_num_no_gauss_list = [x for x in self.feats_num_list if x not in feats_num_gauss_list]
 
-            print("List post-Num feats: {}".format(len(self.feats_num_list)))
-            print("List post-Num-Gauss feats: {}".format(len(feats_num_gauss_list)))
+            self.logger.debug("List post-Num feats: {}".format(len(self.feats_num_list)))
+            self.logger.debug("List post-Num-Gauss feats: {}".format(len(feats_num_gauss_list)))
 
             # load normalization pipeline
             # full_pipeline = joblib.load(os.path.join(exports_dir, "full_pipeline_{}.pkl".format(self.process)))
@@ -138,12 +152,12 @@ class TransformPredictions:
             select_rename_list = columns[:len(self.feats_num_list)]
             select_rename_list = self.feats_num_list
             select_no_rename_list = columns[len(self.feats_num_list):]
-            print(select_no_rename_list)
+            self.logger.debug("Selected no rename list: {}".format(select_no_rename_list))
             new_feats_columns = select_rename_list + select_no_rename_list
             self.df_feats.columns = new_feats_columns
-            print("Normalized Features DF:")
-            print(self.df_feats)
-            print("Shape: {}".format(self.df_feats.shape))
+            self.logger.debug("Normalized Features DF:")
+            self.logger.debug("{}".fomrat(self.df_feats))
+            self.logger.debug("Shape: {}".format(self.df_feats.shape))
             # feats_no_gauss_list = [x for x in new_feats_columns if x not in feats_num_gauss_list]
 
             # load guassianization pipeline
